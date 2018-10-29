cc24b0f1d18d2f03dafd446e51e2e1efc527bce3
Michael Harrison <michael.harrison@peak.ai>
2018-10-29 19:31:08 +0000

    Add app component

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>The News</h1>
+        </header>
+        <main>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
