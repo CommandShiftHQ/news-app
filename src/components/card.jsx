3286467ad7ec1e6b5d252bee894bf521cc5612e1
Michael Harrison <michael.harrison@peak.ai>
2018-10-29 21:41:22 +0000

    Styling 💅

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import './card.scss';
+import bestInShow from '../best-in-show.jpg';
 
 const Card = (props) => (
   <article>
     <img src={props.image} alt="" />
-    <div>
-      <div>
+    <div className="barry">
+      <div className="roland">
         <h2>{props.title}</h2>
-        <p>{props.section}</p>
+        <span>{props.section}</span>
       </div>
-      <div>
+      <div className="roland">
         <p>{ReactHtmlParser(props.blurb)}</p>
       </div>
-      <div>
+      <div className="roland">
         <a href="#">More</a>
       </div>
     </div>
   </article>
 );
+
+Card.defaultProps = {
+  image: bestInShow,
+};
 
 export default Card;
